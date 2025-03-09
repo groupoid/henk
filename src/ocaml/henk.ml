bc2db3ca0dd9991f20ed48328b97139893a7cdfc
5HT <maxim@synrc.com>
2025-03-09 04:00:49 +0200

    OCaml Henk

--- src/ocaml/henk.ml
+++ src/ocaml/henk.ml
@@ -36,11 +36,9 @@ let rec string_of_term = function
 let rec equal ctx t1 t2 =
     let t1' = normalize ctx t1 in
     let t2' = normalize ctx t2 in
-    let t1_ty = infer ctx t1' in
-    let t2_ty = infer ctx t2' in
     match t1', t2' with
-    | Lam (x, d, b), t when not (is_lam t) -> (match t2_ty with | Pi _ -> equal' ctx t1' t2' | _ -> false)
-    | t, Lam (x, d, b) when not (is_lam t) -> (match t1_ty with | Pi _ -> equal' ctx t1' t2' | _ -> false)
+    | Lam (x, d, b), t when not (is_lam t) -> (match infer ctx t1' with | Pi _ -> equal' ctx t1' t2' | _ -> false)
+    | t, Lam (x, d, b) when not (is_lam t) -> (match infer ctx t2' with | Pi _ -> equal' ctx t1' t2' | _ -> false)
     | _ -> equal' ctx t1' t2'
 
 and equal' ctx t1 t2 =
